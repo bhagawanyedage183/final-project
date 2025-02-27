1cc6e26d83a7427a5a06b904082ec86c933db210
bhagawanyedage183 <bhagwanyedage6@gmail.com>
2025-02-27 11:34:12 +0530

    Feature added

---
+++ Backend/index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const cors = require('cors');
+const path = require('path');
+var jwt = require('jsonwebtoken');
+const multer = require('multer');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose")
+
+
+dotenv.config();
+
+const productController = require('./controllers/productController');
+const userController = require('./controllers/userController');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+})
+
+const upload = multer({ storage: storage })
+const bodyParser = require('body-parser')
+const app = express()
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+const port = process.env.PORT || 4000;
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(console.log("Connected to MongoDB"))
+    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+
+    app.get('/', (req, res) => {
+    res.send('hello...')
+})
+
+app.get('/search', productController.search)
+app.post('/like-product', userController.likeProducts)
+app.post('/add-product', upload.fields([{ name: 'pimage' }, { name: 'pimage2' }]), productController.addProduct)
+app.get('/get-products', productController.getProducts)
+app.get('/get-product/:pId', productController.getProductsById)
+app.post('/liked-products', userController.likedProducts)
+app.post('/my-products', productController.myProducts)
+app.post('/signup', userController.signup)
+app.get('/my-profile/:userId', userController.myProfileById)
+app.get('/get-user/:uId', userController.getUserById)
+app.post('/login', userController.login)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
